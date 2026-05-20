0dceb3f11cc349bad27ed275de49a6b0d752feee
Shahina Begam <9115shahinabegam@gmail.com>
2026-05-20 12:15:30 +0530

    Removed hindi translation

--- src/translation/index.js
+++ src/translation/index.js
@@ -4,11 +4,6 @@ const translations = {
     Logindetails: "Please login or signup to manage your expenses."
   },
 
-  hi: {
-    ExpenseFlow: "एक्सपेंस फ्लो",
-    Logindetails: "अपने खर्चों को प्रबंधित करने के लिए कृपया लॉगिन या साइनअप करें।"
-  },
-
   jp: {
     ExpenseFlow: "エクスペンスフロー",
     Logindetails: "支出を管理するには、ログインまたはサインアップしてください。"
